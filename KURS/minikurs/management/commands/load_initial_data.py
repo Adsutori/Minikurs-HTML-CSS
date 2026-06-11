7a86ac67ee2a5a4a034d552df390b781c37f8c99
Adsutori <adsutori@outlook.com>
2026-06-12 01:07:46 +0200

    add load_initial_data and dane.json

---
+++ KURS/minikurs/management/commands/load_initial_data.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+from minikurs.models import Kurs  # ← zmień na swój główny model
+
+
+class Command(BaseCommand):
+    help = 'Ładuje dane tylko jeśli baza jest pusta'
+
+    def handle(self, *args, **kwargs):
+        if Kurs.objects.exists():
+            self.stdout.write('Dane już istnieją — pomijam.')
+            return
+        call_command('loaddata', 'dane.json')
+        self.stdout.write(self.style.SUCCESS('Dane załadowane!'))
